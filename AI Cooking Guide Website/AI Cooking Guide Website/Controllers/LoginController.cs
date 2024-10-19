7ba8c104526d6f71a680595d89f4038e8d18ef36
dduc2550 <dduc2550@gmail.com>
2024-10-19 19:11:02 +0700

    update giao dien

---
+++ AI Cooking Guide Website/AI Cooking Guide Website/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AI_Cooking_Guide_Website.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
