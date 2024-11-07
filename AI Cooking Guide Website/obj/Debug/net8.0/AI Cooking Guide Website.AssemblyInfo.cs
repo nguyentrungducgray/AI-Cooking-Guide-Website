89a03e0d6306ae32a4a066df9436c595ec43c293
nguyentrungducgray <trungduc2004k54@gmail.com>
2024-11-07 10:11:44 +0700

    gitinore

--- AI Cooking Guide Website/obj/Debug/net8.0/AI Cooking Guide Website.AssemblyInfo.cs
+++ AI Cooking Guide Website/obj/Debug/net8.0/AI Cooking Guide Website.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI Cooking Guide Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e9fc23a4ef288b0d16c4df054790d33f7e40884")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2fd82c19ed3b6d1f378e4472b063d689669da5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI Cooking Guide Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI Cooking Guide Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
