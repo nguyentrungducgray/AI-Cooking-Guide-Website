07e71e20de513854e99efc97b230586213e96d8f
dduc2550 <dduc2550@gmail.com>
2024-11-21 08:47:03 +0700

    Add mon an

--- AI Cooking Guide Website/obj/Debug/net8.0/AI Cooking Guide Website.AssemblyInfo.cs
+++ AI Cooking Guide Website/obj/Debug/net8.0/AI Cooking Guide Website.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI Cooking Guide Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+769ed81bae09c9f555b35fa3c09dd1a3b1992bc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83aa409c51ceded8e5f2a87bcbd7ae111d153a5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI Cooking Guide Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI Cooking Guide Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
