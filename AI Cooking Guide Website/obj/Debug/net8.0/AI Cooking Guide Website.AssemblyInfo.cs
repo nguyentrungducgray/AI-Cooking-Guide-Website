d4852a90526daf7d43e6a8b692b590d2214e6c37
unknown <phuongdiemtranthi23@gmail.com>
2024-12-04 14:43:31 +0700

    updateVIE_admin

--- AI Cooking Guide Website/obj/Debug/net8.0/AI Cooking Guide Website.AssemblyInfo.cs
+++ AI Cooking Guide Website/obj/Debug/net8.0/AI Cooking Guide Website.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI Cooking Guide Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37a10a48cd524c857f23d4eab856109a31a4999c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23e95e3e251ce818901d10e71f24649436cb5e89")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI Cooking Guide Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI Cooking Guide Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
